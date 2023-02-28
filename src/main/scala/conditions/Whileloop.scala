54d0adcc5d771e5e79ac9682ee28998648a2c235
ranirk <nagaranigk@gmail.com>
2023-02-28 11:51:25 +0000

    Initial commit

---
+++ src/main/scala/conditions/Whileloop.scala
@@ -0,0 +1,12 @@
+package conditions
+
+object Whileloop {
+  def main(args: Array[String]): Unit = {
+    var a=0;
+    while(a<10) {
+      println("Value of a is:" +a)
+      a +=1;
+    }
+  }
+
+}
---
+++ src/main/scala/conditions/dowhile.scala
@@ -0,0 +1,14 @@
+package conditions
+
+object dowhile {
+  def main(args: Array[String]){
+    var x=5;
+    do{
+      println("value of x is:" +x)
+      x +=1;
+    }
+    while(x<10)
+
+  }
+
+}
